66ba2e8f74e3429a45da7ddc8ca5f8c5eae6fae4
jadman02 <jadman02@hotmail.com>
2014-09-12 22:40:54 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -38,12 +38,12 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
+
 }
 else {
 	
 	var anotherView = myApp.addView('.another-view');
 	anotherView.hideNavbar();
-	mainView.loadPage('register.html');
 	
 	
 }
