486f29d789e8cfe75cbc512f55ac9dde71b41157
jadman02 <jadman02@hotmail.com>
2014-09-14 21:12:38 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -75,7 +75,14 @@ myApp.onPageInit('about', function (page) {
 });
 
 
+myApp.onPageInit('location', function (page) {
 
+alert('on location page');
+$( '#places_button' ).change(function() {
+  alert( 'Handler for .change() called.' );
+});
+
+});
 
 
 
