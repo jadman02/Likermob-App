4c9fd3be22007050e15a42141095e4699eec6019
jadman02 <jadman02@hotmail.com>
2014-09-16 11:38:43 +1000

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -47,9 +47,10 @@ var mainView = myApp.addView('.view-main', {
 var register = localStorage.getItem('register');
 if (register) {
 
-
+if(latitude) {}
+else{mainView.loadPage('location.html');}
 	
-mainView.loadPage('location.html');	
+	
 
 
 
